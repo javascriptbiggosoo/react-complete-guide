b872ec57bce9e135566bb20f869931cc1331cb97
KIM JINYOUNG <tbs01215@gmail.com>
2022-07-31 16:47:45 +0900

    02 done

--- 02-react-state-events/src/components/Expenses/ExpenseItem.js
+++ 02-react-state-events/src/components/Expenses/ExpenseItem.js
@@ -1,8 +1,12 @@
-import ExpenseDate from "./ExpenseDate";
-import "./ExpenseItem.css";
-import Card from "../UI/Card";
+import React, { useState } from "react";
 
-function ExpenseItem(props) {
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+import "./ExpenseItem.css";
+
+const ExpenseItem = (props) => {
+  // function clickHandler() {}
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -10,9 +14,8 @@ function ExpenseItem(props) {
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button>Change Title</button>
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
