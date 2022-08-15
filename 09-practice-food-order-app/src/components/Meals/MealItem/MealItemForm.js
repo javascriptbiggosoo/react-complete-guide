a17d7c242a2f7aa1fd89a57963348fded9d5815b
KIM JINYOUNG <tbs01215@gmail.com>
2022-08-15 13:49:21 +0900

    09 done

--- 09-practice-food-order-app/src/components/Meals/MealItem/MealItemForm.js
+++ 09-practice-food-order-app/src/components/Meals/MealItem/MealItemForm.js
@@ -1,10 +1,33 @@
+import { useRef, useState } from "react";
 import Input from "../../UI/Input";
 import classes from "./MealItemForm.module.css";
 
 const MealItemForm = (props) => {
+  const [amountIsValid, setAmountIsValid] = useState(true);
+  const amountInputRef = useRef();
+
+  const submitHandler = (ev) => {
+    ev.preventDefault();
+
+    const enteredAmount = amountInputRef.current.value;
+    const enteredAmountNumber = +enteredAmount;
+
+    if (
+      enteredAmount.trim().length === 0 ||
+      enteredAmountNumber < 1 ||
+      enteredAmountNumber > 5
+    ) {
+      setAmountIsValid(false);
+      return;
+    }
+
+    props.onAddToCart(enteredAmountNumber);
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <Input
+        ref={amountInputRef}
         label="Amount"
         input={{
           id: `amount_${props.id}`,
@@ -15,6 +38,7 @@ const MealItemForm = (props) => {
         }}
       ></Input>
       <button>+ Add</button>
+      {!amountIsValid && <p>주문 수량을 다시 확인해주세요(최대 5)</p>}
     </form>
   );
 };
