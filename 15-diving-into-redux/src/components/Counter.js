66af55051d29f9cb3a559271c50be7fdfc880ac0
KIM JINYOUNG <tbs01215@gmail.com>
2022-09-07 16:06:34 +0900

    15 done

--- 15-diving-into-redux/src/components/Counter.js
+++ 15-diving-into-redux/src/components/Counter.js
@@ -1,12 +1,42 @@
-import classes from './Counter.module.css';
+import { useSelector, useDispatch } from "react-redux";
+
+import { counterActions } from "../store/counter";
+import classes from "./Counter.module.css";
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+  const dispatch = useDispatch();
+  const counter = useSelector((state) => state.counter.counter);
+  const show = useSelector((state) => state.counter.showCounter);
+
+  const incrementHandler = () => {
+    dispatch(counterActions.increment());
+  };
+
+  const increaseHandler = () => {
+    // 여기 넣은 인자는 increase의 action.payload 프로퍼티로 가도록 되어있다.
+    dispatch(counterActions.increase(10));
+
+    console.log(counterActions.increase(10));
+    // {type: 'counter/increase', payload: 10} 이게 action object로 디스패치된다.
+  };
+
+  const decrementHandler = () => {
+    dispatch(counterActions.decrement());
+  };
+
+  const toggleCounterHandler = () => {
+    dispatch(counterActions.toggleCount());
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      {show && <div className={classes.value}>{counter}</div>}
+      <div>
+        <button onClick={incrementHandler}>증가</button>
+        <button onClick={increaseHandler}>10씩 증가</button>
+        <button onClick={decrementHandler}>감소</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
